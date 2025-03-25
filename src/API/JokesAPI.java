bdf3b25478aec6aa4c3725e711ba38d8ab573b87
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-25 22:54:19 +0200

    creating request

---
+++ src/API/JokesAPI.java
@@ -0,0 +1,23 @@
+package API;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class JokesAPI {
+    public static HttpClient client;
+
+    public static void requestAPI() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://official-joke-api.appspot.com/random_joke")) //endpoint
+                .timeout(Duration.ofSeconds(5)) //timeout for request
+                .header("Content-type", "application/json") //header parameters
+                .GET() //method
+                .build(); //mandatory to execute request
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println);
+    }
+}
+
---
+++ src/Run.java
@@ -0,0 +1,7 @@
+import API.JokesAPI;
+
+public class Run {
+    public static void main(String[] args) {
+        JokesAPI.requestAPI();
+    }
+}
