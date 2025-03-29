0b739c481abdc077ab2136bee29e44ce33c83d8a
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-29 16:38:03 +0200

    introducing controller, mainWindow

---
+++ src/application/Controller.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.fxml.FXML;
+import java.awt.*;
+
+public class Controller {
+
+    @FXML
+    private Button btnGetJoke;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnViewSavedJokes;
+
+    @FXML
+    private TextArea fieldJoke;
+}
