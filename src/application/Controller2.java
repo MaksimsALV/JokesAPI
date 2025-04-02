3192b33efe4ba6730f7231736d54adc61534c26e
MaksimsALV <maxim.ahmetov@gmail.com>
2025-04-02 19:46:06 +0300

    fixing UI

--- src/application/Controller2.java
+++ src/application/Controller2.java
@@ -8,8 +8,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.time.Instant;
-
 public class Controller2 {
 
     @FXML
